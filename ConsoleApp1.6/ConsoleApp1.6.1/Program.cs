c5729e35a6a1b25dfdbd3a2b3998bdafaffab5ad
lover_piva <118905877+xaku13@users.noreply.github.com>
2023-12-08 18:40:37 +0700

    fix 1.0.4

--- ConsoleApp1.6/ConsoleApp1.6.1/Program.cs
+++ ConsoleApp1.6/ConsoleApp1.6.1/Program.cs
@@ -7,7 +7,7 @@ namespace App1
     {
         public static void Main(string[] args)
         {
-            string path = @"C:\Users\gr622_nemol\Desktop\practica\ConsoleApp1.6\ConsoleApp1.6.1\bin\Debug\net7.0\numsTask1.txt";
+            string path = @"numsTask1.txt";
             StreamReader readTask5 = new StreamReader(path);
             string[] words = readTask5.ReadLine().Split(' ');
             
